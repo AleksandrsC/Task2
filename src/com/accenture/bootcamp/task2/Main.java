34b0dca5f14f7d425a2dc893433918e0306b6f09
AleksandrsC <mirrorfrequency@gmail.com>
2022-03-22 16:27:43 +0200

    task 2 problem 6

--- src/com/accenture/bootcamp/task2/Main.java
+++ src/com/accenture/bootcamp/task2/Main.java
@@ -5,17 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    private static BufferedReader console=new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
-	    BufferedReader console=new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("String:");
+
         try {
-            String inp = console.readLine();
-            System.out.println();
-            System.out.println("input:" + inp);
+            int a=Integer.parseInt(askUser("number A:"));
+            int b=Integer.parseInt(askUser("number B:"));
+            System.out.printf("result:%s%n", a == b || (a < 0 && b > 0) || (a > 1001 && b > 1001));
         }catch(IOException x){
             System.err.println(x);
         }
 
+    }
+
+    private static String askUser(String prompt) throws IOException {
+        System.out.print(prompt);
+        return console.readLine();
     }
 }
