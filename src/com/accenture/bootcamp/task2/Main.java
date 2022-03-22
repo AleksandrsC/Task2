9f39aca16385418262087c0a27462de645b034ad
AleksandrsC <mirrorfrequency@gmail.com>
2022-03-22 16:07:09 +0200

    basic input/output demo

--- src/com/accenture/bootcamp/task2/Main.java
+++ src/com/accenture/bootcamp/task2/Main.java
@@ -1,8 +1,21 @@
 package com.accenture.bootcamp.task2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    BufferedReader console=new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("String:");
+        try {
+            String inp = console.readLine();
+            System.out.println();
+            System.out.println("input:" + inp);
+        }catch(IOException x){
+            System.err.println(x);
+        }
+
     }
 }
